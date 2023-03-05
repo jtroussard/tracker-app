652573bbf9f7cbe7b493788d56b373e36112a4e1
jacques <jacques.troussard@gmail.com>
2023-03-05 18:39:36 -0500

    3: adds tracker route and basic tracker template

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
+login_name = 'Michael Scott'
 
 @app.route('/')
 def home():
-    login_name = 'Michael Scott'
     return render_template('home.html', active_page='home', login_name=login_name)
+
+@app.route('/tracker')
+def tracker():
+    return render_template('tracker.html', active_page='tracker', login_name=login_name)
 
 if __name__ == '__main__':
     app.run()
