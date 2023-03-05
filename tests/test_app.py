128f96346ee6ac82668de24834646e26314428ac
jacques <jacques.troussard@gmail.com>
2023-03-05 13:45:02 -0500

    1: removed duplicate test and commented code

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,3 @@
-def test_home_page(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"Hello, World!" in response.data
-
 def test_hello(client):
     response = client.get('/')
     assert response.status_code == 200
@@ -13,6 +8,3 @@ def test_main(client):
     print('tuna')
     print(app.name)
     assert app.name == 'app'
-    # assert app.app == app.Flask(__name__)
-    # with app.app.test_client() as client:
-    #     assert client.get('/').status_code == 200
