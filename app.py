d0d190cbc003002aa972a1fb4afba3683904b3fe
jacques <jacques.troussard@gmail.com>
2023-03-05 16:23:45 -0500

    8: adds home, layout, navbar, bootstrap CDN and some styling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from config import Config
 
 app = Flask(__name__)
@@ -6,8 +6,9 @@ app.config.from_object(Config)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def home():
+    login_name = 'Michael Scott'
+    return render_template('home.html', active_page='home', login_name=login_name)
 
 if __name__ == '__main__':
     app.run()
