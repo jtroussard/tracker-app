ab688800f6276c1823695d2729d046a1d3723453
jacques <jacques.troussard@gmail.com>
2023-03-05 19:38:34 -0500

    2: adds tests

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 class Config(object):
     SERVER_NAME = 'localhost:12031'
     DEBUG = True
+    BASE_URL = 'http://localhost:12031'
+
+class TestingConfig(object):
+    BASE_URL = 'http://localhost:12031'
