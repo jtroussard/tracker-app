db9461c0790129023af4808ac30537c4c37dd7b8
jacques <jacques.troussard@gmail.com>
2023-03-05 19:12:39 -0500

    2: adds basic templates and routes for login, logout, register

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from config import Config
 
 app = Flask(__name__)
@@ -13,6 +13,22 @@ def home():
 @app.route('/tracker')
 def tracker():
     return render_template('tracker.html', active_page='tracker', login_name=login_name)
+
+@app.route('/login')
+def login():
+    # do things to log in the user
+    return render_template('login.html', active_page='login', login_name=login_name)
+
+@app.route('/logout')
+def logout():
+    # do things to log out the user
+    print('user logged out')
+    return redirect(url_for('home'))
+
+@app.route('/register')
+def register():
+    # do things to register a new user
+    return render_template('register.html', active_page='register', login_name=login_name)
 
 if __name__ == '__main__':
     app.run()
