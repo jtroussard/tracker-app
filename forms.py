277a7f997f189af4e3ae7076245118a0e530a671
jacques <jacques.troussard@gmail.com>
2023-03-11 18:39:45 -0500

    35: adds entry form, entry tile, and stubs the form submition

--- forms.py
+++ forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateField, SelectField, FloatField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from choices import TIME_OF_DAY_CHOICES, MOOD_CHOICES, STATUS_CHOICES
+from validators import validate_weight
 
+import datetime
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -20,3 +23,13 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class TrackerEntryForm(FlaskForm):
+    date = DateField('Date', validators=[DataRequired()], default=datetime.date.today)
+    time_of_day = SelectField('Time of Day', choices=TIME_OF_DAY_CHOICES, validators=[DataRequired()])
+    mood = SelectField('Mood', choices=MOOD_CHOICES)
+    status = SelectField('Status', choices=STATUS_CHOICES)
+    weight = FloatField('Weight', validators=[validate_weight, DataRequired()])
+    measurement_waist = FloatField('Waist')
+    keto = IntegerField('Ketosis Level')
+    submit = SubmitField('Save')
---
+++ validators.py
@@ -0,0 +1,25 @@
+from wtforms.validators import ValidationError
+
+def validate_weight2(form, field):
+    print(type(field.data))
+    print(field.data)
+    print('TUNA')
+    if field.data is not None:
+        whole_number = int(field.data)
+        decimal_places = len(str(field.data).split('.')[1]) if '.' in str(field.data) else 0
+        if whole_number > 999 or decimal_places > 1:
+            raise ValidationError('Weight must be less than or equal to 999 and have only one decimal place')
+        
+def validate_weight(form, field):
+    if field.data is not None:
+        whole_number = None
+        decimal_places = None
+        if isinstance(field.data, (int, float)):
+            whole_number = int(field.data)
+            decimal_places = len(str(field.data).split('.')[1]) if '.' in str(field.data) else 0
+        else:
+            raise ValidationError('Weight must be a number')
+
+        if whole_number > 999 or decimal_places > 1:
+            raise ValidationError('Weight must be less than or equal to 999 and have only one decimal place')
+
