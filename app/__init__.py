ace14896ea8fc7871fc7977eab8805049b9fd3bd
Jacques Troussard <jacques.troussard@gmail.com>
2023-05-21 14:09:10 -0400

    Deployment fixes and logging (#101)
    * adds logger and env example file and deployment fixes
    
    * installs logging dependencies
    
    * moves logger init

--- app/__init__.py
+++ app/__init__.py
@@ -31,7 +31,7 @@ def setup_logger():
     logger.setLevel(logging.INFO)
 
     # Create a rotating file handler to log messages to a file
-    file_handler = RotatingFileHandler('app.log', maxBytes=1024 * 1024 * 10, backupCount=5)
+    file_handler = RotatingFileHandler('tracker-app-logger.log', maxBytes=1024 * 1024 * 10, backupCount=5)
     file_handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(module)s - %(message)s'))
     logger.addHandler(file_handler)
 
@@ -69,6 +69,7 @@ def create_app(config_class):
         The Flask application.
     """
     app = Flask(__name__)
+    logger = setup_logger()
     app.config.from_object(config_class)
     logger.info(f"Loaded config class: {config_class}")
     register_filters(app)
